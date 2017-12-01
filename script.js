94ba2d522e25d91fb23a81618ce9563d424581a2
Enzo Barrett <32809700+enzobarrett@users.noreply.github.com>
2017-12-01 13:04:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,6 @@ document.onmousemove=function(e){
   var jack = document.getElementById('jack');
   if (e) {x=e.clientX; y=e.clientY;}
   else {x=event.clientX;y=event.clientY;}
-  if (x === 0) {jack.style.visibility=none;}
-  else {jack.style.visibility=visible;}
   jack.style.top=y+'px';
   jack.style.left=x-90+'px';
   }};
