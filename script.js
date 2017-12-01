8beefd66b3901b6c5dec9180104864624d83c042
Enzo Barrett <32809700+enzobarrett@users.noreply.github.com>
2017-12-01 08:46:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,8 @@ document.onmousemove=function(e){
   var x, y;
   if (e) {x=e.clientX; y=e.clientY;} 
   else {x=event.clientX;y=event.clientY;}
-
-var jack = document.getElementById('jack');
-jack.style.top=y+'px';
-jack.style.left=x+50+'px';
-});
+  var jack = document.getElementById('jack');
+  jack.style.top=y+'px';
+  jack.style.left=x+50+'px';
+  }};
 
