347e26079de5863af552d8d0bc29b2a5690bbc82
Enzo Barrett <32809700+enzobarrett@users.noreply.github.com>
2017-11-30 23:06:20 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var div = document.createElement("div");
-document.getElementsByTagName('div').innerHTML = '<img alt="pickle" src="images/pickle">';
+div.innerHTML = '<img alt="pickle" src="images/pickle">';
 div.style.visibility = "visible";
 div.style.position = "absolute";
 div.style.zIndex = "3";
