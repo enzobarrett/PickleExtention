8c4c13b30a7fedf5e537c5e81ae5ec6ac60a9fe0
Enzo Barrett <elbarrett01@bvsd.org>
2017-12-01 15:32:51 -0700

    update hidden image if x=0

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ document.body.appendChild(div);
 document.onmousemove=function(e){
   var x, y;
   var jack = document.getElementById('jack');
-  if (e) {x=e.clientX; y=e.clientY;}
-  else {x=event.clientX;y=event.clientY;}
+  if (e) {x=e.clientX; y=e.clientY;} else {x=event.clientX;y=event.clientY;};
+  if (x = 0) {jack.style.visibility: hidden;} else {jack.style.visibility: visible;};
+
   jack.style.top=y+'px';
   jack.style.left=x-90+'px';
   }};
