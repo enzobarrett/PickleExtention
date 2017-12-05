4368113487512b5ff64a5474ad95e1ec09449de6
Enzo Barrett <elbarrett01@bvsd.org>
2017-12-05 11:07:16 -0700

    comment out not working parts

--- script.js
+++ script.js
@@ -9,15 +9,16 @@ div.style.zIndex = "20000";
 div.className = "jack";
 div.id = "jack";
 document.body.appendChild(div);
-document.getScroll= function(){
+/*document.getScroll= function(){
   var scroll = element.scrollTop;
   };
+  */
 document.onmousemove=function(e){
   var x, y;
   var jack = document.getElementById('jack');
   if (e) {x=e.clientX; y=e.clientY;} else {x=event.clientX;y=event.clientY;}
   if (y <= 10) {jack.style.visibility="hidden";} else {jack.style.visibility="visible";}
-  jack.style.top=y+scroll+'px';
+  jack.style.top=y/*+scroll*/+'px';
   jack.style.left=x-85+'px';
   console.log(scroll);
   }};
