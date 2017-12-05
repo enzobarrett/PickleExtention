050d27bff6e677e0ff84bde681da2d904931adfd
Enzo Barrett <elbarrett01@bvsd.org>
2017-12-05 11:01:55 -0700

    add var scroll

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function() {
 var div = document.createElement("div");
+var scroll;
 div.innerHTML = '<img alt="pickle" src="">';
 div.firstElementChild.src = chrome.extension.getURL("images/Pickle_rick_transparent4.png");
 div.style.visibility = "visible";
@@ -8,13 +9,15 @@ div.style.zIndex = "20000";
 div.className = "jack";
 div.id = "jack";
 document.body.appendChild(div);
+document.getScroll= function(){
+  scroll = element.scrollTop;
+  };
 document.onmousemove=function(e){
   var x, y;
   var jack = document.getElementById('jack');
   if (e) {x=e.clientX; y=e.clientY;} else {x=event.clientX;y=event.clientY;}
   if (y <= 10) {jack.style.visibility="hidden";} else {jack.style.visibility="visible";}
-  jack.style.top=y+'px';
+  jack.style.top=y+scroll+'px';
   jack.style.left=x-85+'px';
-  console.log(x);
-  console.log(y);
+  console.log(scroll);
   }};
